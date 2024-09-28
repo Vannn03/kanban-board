5aab3b93f98f0cb4da3e6bffaec5b26f6e296ea0
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2024-09-28 22:13:47 +0700

    add: data fetch and display, basic UI design with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,175 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
+import axios from "./utils/axiosInstance";
+import { FaPlus } from "react-icons/fa";
+import { IoIosMore } from "react-icons/io";
+import { FaCircleCheck } from "react-icons/fa6";
+import { FiPlusCircle } from "react-icons/fi";
 
-function App() {
-    const [count, setCount] = useState(0);
+type TodosType = {
+    id: number;
+    title: string;
+    created_by: string;
+    created_at: Date;
+    updated_at: Date;
+    description?: string;
+};
+
+type ItemsType = {
+    id: number;
+    name: string;
+    done?: boolean;
+    todo_id: number;
+    created_at: Date;
+    updated_at: Date;
+    progress_percentage: number;
+};
+
+const App = () => {
+    const [todos, setTodos] = useState<TodosType[]>([]);
+    const [items, setItems] = useState<Record<number, ItemsType[]>>({});
+
+    // Fetch all Todos
+    const fetchTodos = useCallback(async () => {
+        try {
+            const res = await axios.get(
+                `${import.meta.env.VITE_RAKAMIN_BASE_URL}/todos`
+            );
+            setTodos(res.data);
+
+            // Fetch items for each todo
+            res.data.forEach((todo: TodosType) => {
+                fetchItems(todo.id);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchTodos();
+    }, [fetchTodos]);
+
+    const fetchItems = async (todo_id: number) => {
+        try {
+            const res = await axios.get(
+                `${
+                    import.meta.env.VITE_RAKAMIN_BASE_URL
+                }/todos/${todo_id}/items`
+            );
+            setItems((prevItems) => ({
+                ...prevItems,
+                [todo_id]: res.data,
+            }));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // Function to get color class based on index
+    const getColorClass = (index: number) => {
+        const colors = [
+            "bg-color-primary/5 text-color-primary border-color-primary",
+            "bg-color-secondary/5 text-color-secondary border-color-secondary",
+            "bg-color-danger/5 text-color-danger border-color-danger",
+            "bg-color-success/5 text-color-success border-color-success",
+        ];
+        return colors[index % colors.length];
+    };
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+            <header className="px-5 py-[18px] flex items-center gap-2 border border-color-black/40 font-bold">
+                <h1 className="text-lg">Product Roadmap</h1>
+                <button className="px-4 py-1 rounded-lg bg-color-primary text-white flex items-center gap-2 text-xs">
+                    <FaPlus />
+                    Add New Group
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            </header>
+            <div className="grid grid-cols-4 gap-4 p-6">
+                {/* DISPLAY ALL TODOS */}
+                {todos?.map((todo, index) => (
+                    <div
+                        key={todo.id}
+                        className={`rounded p-4 border flex flex-col gap-2 h-fit ${getColorClass(
+                            index
+                        )}`}
+                    >
+                        <h1
+                            className={`border rounded px-2 py-[2px] w-fit ${getColorClass(
+                                index
+                            )} bg-opacity-0`}
+                        >
+                            {todo.title}
+                        </h1>
+                        <p className="text-xs font-bold text-color-black/90">
+                            {todo.description}
+                        </p>
+                        <div className="grid grid-cols-1 gap-3">
+                            {/* DISPLAY ALL ITEMS IN EACH TODO */}
+                            {items[todo.id]?.length ? (
+                                items[todo.id].map((item) => (
+                                    <div
+                                        key={item.id}
+                                        className="rounded border p-4 flex flex-col gap-2 bg-color-black/5 border-color-black/15"
+                                    >
+                                        <h3 className="font-bold text-sm text-color-black">
+                                            {item.name}
+                                        </h3>
+                                        <hr className="border border-dashed border-color-black/15" />
+                                        <div className="flex justify-between items-center">
+                                            <div className="flex items-center gap-[12px]">
+                                                {item.progress_percentage ===
+                                                100 ? (
+                                                    <>
+                                                        <progress
+                                                            value={
+                                                                item.progress_percentage
+                                                            }
+                                                            max="100"
+                                                            className="completed"
+                                                        ></progress>
+                                                        <FaCircleCheck className="text-color-success" />
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <progress
+                                                            value={
+                                                                item.progress_percentage
+                                                            }
+                                                            max="100"
+                                                            className="onprogress"
+                                                        ></progress>
+                                                        <p className="text-xs text-color-black">
+                                                            {
+                                                                item.progress_percentage
+                                                            }
+                                                            %
+                                                        </p>
+                                                    </>
+                                                )}
+                                            </div>
+                                            <IoIosMore className="text-2xl text-color-black/70" />
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className="rounded border px-4 py-2 bg-color-black/5 border-color-black/15">
+                                    <p className="text-sm text-color-black">
+                                        No tasks
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                        <button className="w-fit flex items-center gap-[5px] text-sm text-color-black">
+                            <FiPlusCircle className="text-xl" />
+                            New Task
+                        </button>
+                    </div>
+                ))}
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
-}
+};
 
 export default App;
